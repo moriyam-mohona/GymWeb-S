f167116f2c780957d0224fcfb5dc21502063e562
moriyam-mohona <moriyammohona@gmail.com>
2024-11-15 20:46:01 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,71 @@
+const { MongoClient, ObjectId } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z3gfp8c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+async function run() {
+  const client = new MongoClient(uri, {
+    serverSelectionTimeoutMS: 5000,
+  });
+
+  try {
+    await client.connect();
+    const database = client.db("GymWeb");
+    const Users = database.collection("Users");
+
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "24h",
+      });
+      res.send({ token });
+    });
+
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(403).send({ message: "No token provided" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "Invalid token" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    app.post("/user", async (req, res) => {
+      try {
+        const user = req.body;
+        const result = await Users.insertOne(user);
+        res.send({
+          message: "User added successfully",
+          insertedId: result.insertedId,
+        });
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Failed to add user." });
+      }
+    });
+
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.error);
